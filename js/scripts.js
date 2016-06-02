86a0a7de3954bcbf4ade2a7c51a577005a796d08
Dan Schafer and Scott Scholz <scottscholzpdx@gmail.com>
2016-06-01 19:55:55 -0700

    Completed full basic arithmetic form calulator.

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,27 @@ $(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
+  });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
   });
 });
